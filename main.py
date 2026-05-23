376b2ed1082e310ed5ab67ebfd7ac68f9770727d
bxckupd <110246450+bxckupd@users.noreply.github.com>
2026-05-23 11:05:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from src.analysis import (
     correlation_analysis,
     toxic_days
 )
+from src.visualization import screen_time_plot
+
 DATA_PATH = "data/daily_usage.csv"
 
 def main():
@@ -24,14 +26,16 @@ def main():
         correlation_analysis(df)
     elif args.command == "toxic":
         toxic_days(df)
+    elif args.command == "plot":
+        screen_time_plot(df)
     elif args.command == "full":
-        print(
-            "ПОЛНЫЙ АНАЛИЗ ДАННЫХ\n"
-        )
+        print("ПОЛНЫЙ АНАЛИЗ ДАННЫХ\n")
         basic_stats(df)
         weekend_analysis(df)
         correlation_analysis(df)
         toxic_days(df)
+        screen_time_plot(df)
+
     else:
         print(
             "Доступные команды:\n"
@@ -39,6 +43,7 @@ def main():
             "weekend\n"
             "correlation\n"
             "toxic\n"
+            "plot\n"
             "full"
         )
 
